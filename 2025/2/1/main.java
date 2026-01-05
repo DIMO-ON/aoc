b1b7d665da10a5297b6c50049551df6bcac38414
DIMO-ON <lukia743@gmail.com>
2026-01-05 18:55:10 +0100

    feat(day 2 part 1): ...

--- 2025/2/1/main.java
+++ 2025/2/1/main.java
@@ -4,11 +4,38 @@ import java.io.IOException;
 
 class Main {
     public void mySol(String[] ranges) {
+        Long res = 0;
         for (int i = 0; i < ranges.length; i += 2) {
-            String start = ranges[i];
-            String end   = ranges[i+1];
-            System.out.println(start + "-" + end);
+            ranges[i  ] = ranges[i  ].replaceAll("^[0]*", "").trim();
+            ranges[i+1] = ranges[i+1].replaceAll("^[0]*", "").trim();
+            Long start = Long.parseLong(ranges[i  ]);
+            Long end   = Long.parseLong(ranges[i+1]);
+            
+            while (start <= end) {
+                res += this.checkId(start);
+                int idlen = this.idLen(start);
+                if (!idlen % 2) start += 1;
+            }
+
+            System.out.printf("%s-%s\n", ranges[i], ranges[i + 1]);
         }
+    }
+
+    int idLen(static Long code) {
+        String id = code.toString();
+        return id.length();
+    }
+
+    Long checkId(static Long code) {
+        String id = code.toString();
+        int len = id.length();
+        String l = id.substring(0, len/2);
+        String r = id.substring(len/2, len);
+
+        if (l.compareTo(r) == 0)
+            return code;
+        else
+            return 0;
     }
 
 
@@ -18,7 +45,7 @@ class Main {
     }
     
     public static void main(String[] args) throws IOException {
-        String a = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
+        String a = "011-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
 
         Main test = new Main();
         String[] c = test.parseElfString(a);
