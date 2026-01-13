35b810a0bae5a9564a94afcb2bb2be3e7b1a666f
dimoon <lukia743gmail.com>
2026-01-13 14:28:54 +0100

    day 3 part 1 first commit

---
+++ 2025/3/1/main.java
@@ -0,0 +1,16 @@
+class Main {
+/*
+ * data una lista di tot numeri, cerca 2 numeri in sequenza che formano il valore piu' grande
+ * ex. [81119] -> 89
+ * ex. [12345] -> 45
+ */
+    public static void main(String[] args) {
+        // String ex = "987654321111111"
+        //             "811111111111119"
+        //             "234234234234278"
+        //             "818181911112111";
+
+        // System.out.println(ex);
+        System.out.println("Hello, World!!");
+    }
+}
