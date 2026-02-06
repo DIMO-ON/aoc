a04f773cfba1e93a93a39b9e8e116892acfb6d9f
DIMO-ON <lukia743@gmail.com>
2026-02-06 16:17:26 +0100

    ...

--- 2025/3/2/main.java
+++ 2025/3/2/main.java
@@ -17,7 +17,7 @@ class Main {
     public void mySol() {
         Long res = 0l;
         for (int i = 0; i < this.input.length; i++) {
-            Long actual = this.largestValue(this.input[i], 12);
+            Long actual = this.largestValue(this.input[i], 4);
             res += actual; 
             System.out.println(actual);
         }
@@ -33,36 +33,45 @@ class Main {
             if (a.charAt(i) >= a.charAt(idxmaxval))
                 idxmaxval = i;
 
-        StringBuilder left = new StringBuilder();
-        // starting from the index of max value to right
-        // build string max if a[idxmaxval + i] > a[len - joltsize + j]
+        StringBuilder max= new StringBuilder();
+        // starting from the index of maxb value to right
+        // build string maxb if a[idxmaxbval + i] > a[len - joltsize + j]
         for (int i = idxmaxval; i < a.length() - joltsize; i++) {
-            if (left.length() > 0 && left.charAt(left.length() - 1) < a.charAt(i))
-                left.deleteCharAt(left.length() - 1);
+            if (max.length() > 0 && max.charAt(max.length() - 1) < a.charAt(i))
+                this.replace(max, a.charAt(i));
 
-            if (a.charAt(i) >= a.charAt(a.length() - joltsize))
-                left.append(a.charAt(i));
+            if (max.length() < joltsize && a.charAt(i) >= a.charAt(a.length() - joltsize))
+                max.append(a.charAt(i));
+
+            // System.out.printf("%c %s\n", a.charAt(i), max.toString());
+
+        }
+
+
+        int count_subs = max.length();
+        for (int i = a.length() - joltsize; i < a.length(); i++) {
+            if (count_subs > 0 && max.length() > 0 && max.charAt(max.length() - 1) < a.charAt(i)) {
+                max.deleteCharAt(max.length() - 1);
+                count_subs--;
+            }
+
+            if (max.length() < joltsize)
+                max.append(a.charAt(i));
+
+            // System.out.printf("%c %s\n", a.charAt(i), max.toString());
+
         }
         
-        StringBuilder right = new StringBuilder(a.substring(a.length() - joltsize));
 
-        for (int i = left.length(); i > 0; i--)
-            this.deleteMin(right);
-
-        String max = left.append(right.toString()).toString();
         // System.out.println(max);
-
         // System.exit(0);
-        return Long.parseLong(max);
+        return Long.parseLong(max.toString());
     }
 
-    void deleteMin(StringBuilder s) {
-        if (s.length() <= 0) return;
-        int idxminval = 0;
-        for (int i = 0; i < s.length(); i++)
-            if (s.charAt(i) < s.charAt(idxminval)) idxminval = i;
-
-        s.deleteCharAt(idxminval);
+    void replace(StringBuilder s, char v) {
+        for (int i = s.length() - 1; s.charAt(i) < v && i >= 0; i--) {
+            s.deleteCharAt(i);
+        }
     }
 
 
@@ -75,7 +84,7 @@ class Main {
 
         ex.mySol();
 
-        Main input = new Main(Files.readString(Path.of("2025/3/2/input.txt")));
-        input.mySol();
+        // Main input = new Main(Files.readString(Path.of("2025/3/2/input.txt")));
+        // input.mySol();
     }
 }
