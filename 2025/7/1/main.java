72d997a9ff021be8e47705a02efbc515b2cc7384
DIMO-ON <lukia743@gmail.com>
2026-04-14 21:35:41 +0200

    d7p1 ...

--- 2025/7/1/main.java
+++ 2025/7/1/main.java
@@ -6,55 +6,53 @@ import java.util.Collections;
 import java.util.Comparator;
 
 class Main {
-	class Tree {
-		private class Beam {
-			private int row;
-			private int col;
-			Beam(int row, int col) {
-				this.row = row;
-				this.col = col;
-			}
-			
-			public int extend(String layer) {
-				return 0;
-			}
+	private int followbeam(int beam, int row, char[][] layers, int rows, int cols) {
+		if (row < 0 || row >= rows) return 0;
+		if (beam < 0 || beam >= cols || layers[row][beam] == '|') return 0;
+		int i  = row;
+		for (; i < rows && layers[i][beam] != '^'; i++) {
+			layers[i][beam] = '|';
 		}
 
-		private ArrayList<Beam> beams;
-		private int splits;
-
-		Tree(int seedcol) {
-			this.beams = new ArrayList<Beam>();
-			Beam seed = new Beam(col, 0);
-			this.beams.add(seed);
-		}
-
-		public void growRoot(String layer) {
-			for (Beam b: this.beams) this.splits += b.extend(layer);
-		}
-
+		System.out.println("======< >======");
+		this.print(layers, rows, cols);
+		
+		if (i >= rows) return 0;
+		
+		return 1 + followbeam(beam - 1, i, layers, rows, cols) +
+			     + followbeam(beam + 1, i, layers, rows, cols);
 	}
-    
-    public Long mySol(String input) {
-		Long finalres = 0l;
+
+	public void print(char[][] s, int rows, int cols) {
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++)
+				System.out.printf("%c", s[i][j]);
+
+			System.out.println();
+		}
+	}
+
+    public int mySol(String input) {
 		String copy = new String(input);
 		String[] layers = copy.split("\n");
+		int rows = layers.length;
+		int cols = layers[0].length();
+
+		char[][] mat = new char[layers.length][];
+		for (int i = 0; i < layers.length; i++) {
+			mat[i] = layers[i].toCharArray();
+		}
 		
 		// find seed
 		int seed = 0;
-		while (seed < layers[0].length() && layers[0].charAt(seed++) == 'S');
+		for (;seed < cols && mat[0][seed] != 'S'; seed++);
 
-		Tree t = new Tree(seed);
-
-		for (int i = 0; i < layers.length; i++)
-			t.growRoot(layers[i]);
-
-		return t.splits;
+		return followbeam(seed, 0, mat, rows, cols);
     }
 
     public static void main(String[] args) throws IOException {
         Main sol = new Main();
-		// String input = Files.readString(Path.of("2025/7/1/input.txt"));
+		String input = Files.readString(Path.of("2025/7/1/input.txt"));
         String example = "";
 		example += ".......S.......\n";
 		example += "...............\n";
@@ -73,9 +71,8 @@ class Main {
 		example += ".^.^.^.^.^...^.\n";
 		example += "...............\n";
 
-        Long totcount  = sol.mySol(example);
-        System.out.printf("tachyon beam is split a total of %d times", totcount);
+        int totcount  = sol.mySol(example);
         // totcount  = sol.mySol(input);
-        // System.out.printf("the grand total is %d of the available ingredient IDs are fresh\n", totcount);
+        System.out.printf("tachyon beam is split a total of %d times", totcount);
     }
 }
